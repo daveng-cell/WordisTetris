60738f5be34a407cc65969651c2aeccc8e24eab3
daveng-cell <dave.ng@sjsu.edu>
2026-05-02 18:16:38 -0700

    May 2: - made edits to PolyominoGenerator, makes only straight line polyominos as of right now, I can implement different shapes after proving that this works

--- src/main/java/com/wordris/wordrisproject/PolyominoGenerator.java
+++ src/main/java/com/wordris/wordrisproject/PolyominoGenerator.java
@@ -11,37 +11,43 @@ enum PolyominoState {
     PRE_SUFFIX, BASE;
 }
 
-enum formation {
-    L,
-    J,
-    O,
-    I,
-    T,
-    Z,
-    S
-}
+// Unsure of whether to do different shapes yet, will do straight lines first
+//enum formation {
+//    L,
+//    J,
+//    O,
+//    I,
+//    T,
+//    Z,
+//    S
+//}
 
 public class PolyominoGenerator {
     private PolyominoState state = PolyominoState.PRE_SUFFIX;
-    private formation form = formation.L;
 
     // TODO: implement a way to store all four letter string of prefixes and suffixes
-    private final String[] prefixBank = new String[0];
-    private final String[] suffixBank = new String[0];
+    private final String[] prefixAndSuffixBank = new String[0];
+    private final String[] baseBank = new String[0];
 
     // TODO: implement the generator
     public Queue<Polyomino> generatePolyominos() {
         return null;
     }
 
-    public void setForm(formation form) {
-        this.form = form;
+//    public void setForm(formation form) {
+//        this.form = form;
+//    }
+
+    // Requires JSON file or whatever kind of file with all prefixes, suffixes and bases to implement
+    // Takes that file and implants them into the Strin banks
+    private void createStringBank() {
+
     }
 
     // TODO: implement the word bank, a way to get from either prefix or suffix String bank, and a random number generator to get a random String
     private String getStringBank() {
-        String randomFour = prefixBank[0];
-        return randomFour;
+        String random = prefixAndSuffixBank[0];
+        return random;
     }
 
     private Polyomino makePoly() {
@@ -54,19 +60,14 @@ public class PolyominoGenerator {
         }
 
         // Each formation means setting x and y coordination will be different: b, c, and d are placed with reference to 'a' as the base
-        switch(form) {
-            case L:
-                polyParts[0].setX(X_MAX / 2 + BASE_GRID);
-                polyParts[1].setX(X_MAX / 2 - BASE_GRID);
-                polyParts[1].setY(BASE_GRID);
-                polyParts[2].setX(X_MAX / 2);
-                polyParts[2].setY(BASE_GRID);
-                polyParts[3].setX(X_MAX / 2 + BASE_GRID);
-                polyParts[3].setY(BASE_GRID);
-            case J:
-
-        }
-
+        positionBlocks(polyParts);
         return new Polyomino(chosenString, polyParts, chosenString.length());
+    }
+
+    private void positionBlocks(Rectangle[] blocks) {
+        // In an array of Rectangles, parts of a polyomino is placed as a straight line, similar to a crossword puzzle
+        for(int i = 0; i < blocks.length; i++) {
+            blocks[i].setX(X_MAX / 2 + (BASE_GRID * i));
+        }
     }
 }

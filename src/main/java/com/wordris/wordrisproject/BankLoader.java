b9dd14535f3eb7e5b71306d8cbdd4ab92b412b1c
Izy <43360734+plaidsweater@users.noreply.github.com>
2026-05-09 16:28:18 -0700

    Update BankLoader.java

--- src/main/java/com/wordris/wordrisproject/BankLoader.java
+++ src/main/java/com/wordris/wordrisproject/BankLoader.java
@@ -2,82 +2,25 @@ package com.wordris.wordrisproject;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.HashSet;
 import java.io.IOException;
 import java.util.*;
 
 public class BankLoader {
-
     // LOAD in simple string sets
-    public static Set<String> loadStringSet(String filePath) {
-
-        Set<String> set = new HashSet<>();
-
-        try (BufferedReader br =
-                     new BufferedReader(new FileReader(filePath))) {
-
+    public static Set<String> loadStringSet(String path) {
+        Set<String> result = new HashSet<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line;
-
             while ((line = br.readLine()) != null) {
-
                 line = line.trim();
-
-                if (line.isEmpty()) continue;
-
-                set.add(line);
+                if (!line.isEmpty()) {
+                    result.add(line);
+                }
             }
-
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed loading file: " + path);
         }
-
-        return set;
-    }
-
-    // Chain bank load
-    public static Map<String, Set<List<String>>> loadChainBank(String filePath) {
-
-        Map<String, Set<List<String>>> map = new HashMap<>();
-
-        map.put("PREFIX", new HashSet<>());
-        map.put("SUFFIX", new HashSet<>());
-
-        try (BufferedReader br =
-                     new BufferedReader(new FileReader(filePath))) {
-
-            String line;
-            String currentSection = "";
-
-            while ((line = br.readLine()) != null) {
-
-                line = line.trim();
-
-                if (line.isEmpty()) continue;
-
-                if (line.equalsIgnoreCase("PREFIX:")) {
-                    currentSection = "PREFIX";
-                    continue;
-                }
-
-                if (line.equalsIgnoreCase("SUFFIX:")) {
-                    currentSection = "SUFFIX";
-                    continue;
-                }
-
-                String[] split = line.split(",");
-
-                List<String> chain = new ArrayList<>();
-
-                for (String s : split) {
-                    chain.add(s.trim());
-                }
-
-                map.get(currentSection).add(chain);
-            }
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return map;
+        return result;
     }
 }

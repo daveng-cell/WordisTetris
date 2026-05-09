bfad2003526b3f6f8bf4c9dbe2eb67d958f7b25c
Izy <43360734+plaidsweater@users.noreply.github.com>
2026-05-09 16:51:00 -0700

    Create ChainBankLoader.java

---
+++ src/main/java/com/wordris/wordrisproject/ChainBankLoader.java
@@ -0,0 +1,90 @@
+package com.wordris.wordrisproject;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Set;
+
+//parsing chanbank.txt
+public class ChainBankLoader {
+  
+    public static void loadChains(
+            String path,
+            Set<String> prefixChains,
+            Set<String> suffixChains) {
+
+        try (BufferedReader br =
+                     new BufferedReader(
+                             new FileReader(path))) {
+
+            String line;
+
+            boolean readingPrefix = false;
+            boolean readingSuffix = false;
+
+            while ((line = br.readLine()) != null) {
+
+                line = line.trim();
+
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                // PREFIX SECTION
+                if (line.equalsIgnoreCase("PREFIX:")) {
+
+                    readingPrefix = true;
+                    readingSuffix = false;
+
+                    continue;
+                }
+
+                // SUFFIX SECTION
+                if (line.equalsIgnoreCase("SUFFIX:")) {
+
+                    readingPrefix = false;
+                    readingSuffix = true;
+
+                    continue;
+                }
+
+                String normalized =
+                        normalizeChain(line);
+
+                if (readingPrefix) {
+                    prefixChains.add(normalized);
+                }
+
+                if (readingSuffix) {
+                    suffixChains.add(normalized);
+                }
+            }
+
+        } catch (IOException e) {
+
+            throw new RuntimeException(
+                    "Failed loading chain bank."
+            );
+        }
+    }
+  
+    private static String normalizeChain(
+            String line) {
+
+        String[] split = line.split(",");
+
+        StringBuilder sb =
+                new StringBuilder();
+
+        for (int i = 0; i < split.length; i++) {
+
+            sb.append(split[i].trim());
+
+            if (i != split.length - 1) {
+                sb.append(",");
+            }
+        }
+
+        return sb.toString();
+    }
+}

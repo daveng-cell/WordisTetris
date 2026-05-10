c61d50b24ddbb4c43afe4212d2a25217b3473fb3
Izy <43360734+plaidsweater@users.noreply.github.com>
2026-05-09 17:00:11 -0700

    Create ScoreCalculator.java

---
+++ src/main/java/com/wordris/wordrisproject/ScoreCalculator.java
@@ -0,0 +1,29 @@
+package com.wordris.wordrisproject;
+
+//score calculation
+public class ScoreCalculator {
+    // Every affix gives 1 point
+    private static final int AFFIX_POINTS = 1;
+    // Combo bonuses
+    private static final int THREE_AFFIX_BONUS = 3;
+    private static final int FOUR_AFFIX_BONUS = 6;
+    private static final int FIVE_PLUS_AFFIX_BONUS = 10;
+
+    //Calculates score based on num of affixes
+    public int calculateScore(
+            ParsedWord word) {
+
+        int affixes = word.getAffixCount();
+        int score = affixes * AFFIX_POINTS;
+
+        // Combo bonuses
+        if (affixes == 3) {
+          score += THREE_AFFIX_BONUS;
+        } else if (affixes == 4) {
+            score += FOUR_AFFIX_BONUS;
+        } else if (affixes >= 5) {
+            score += FIVE_PLUS_AFFIX_BONUS;
+        }
+        return score;
+    }
+}

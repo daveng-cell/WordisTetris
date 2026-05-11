d4befd35e23c6a479832313df20540a55dc95a50
Izy <43360734+plaidsweater@users.noreply.github.com>
2026-05-11 10:04:49 -0700

    Update BankLoader.java

--- src/main/java/com/wordris/wordrisproject/BankLoader.java
+++ src/main/java/com/wordris/wordrisproject/BankLoader.java
@@ -1,8 +1,6 @@
 package com.wordris.wordrisproject;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -26,7 +24,6 @@ public class BankLoader {
                 "Failed to load bank: " + resourcePath, e
             );
         }
-
         return words;
     }
 }
